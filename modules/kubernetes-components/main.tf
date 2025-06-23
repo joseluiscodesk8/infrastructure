3668ef7651a4be974e0822f61a7e6bdffc9577f6
Jose Luis <jlgomezangarita@gmail.com>
2025-06-22 21:27:18 -0500

    feat(upgrade): argocd

--- modules/kubernetes-components/main.tf
+++ modules/kubernetes-components/main.tf
@@ -11,7 +11,24 @@ resource "helm_release" "argocd" {
   chart      = "argo-cd"
   version    = "8.1.0" # Usa la última versión estable
 
-  values = [file("${path.module}/resources/argocd-values.yaml")] 
+   values = [templatefile("${path.module}/resources/argocd-values.yaml", {
+    github_client_id     = var.argocd_github_client_id,
+    github_client_secret = var.argocd_github_client_secret
+  })]
   depends_on = [kubernetes_namespace.argocd]
+
 }
 
+resource "kubernetes_manifest" "argocd_root" {
+  field_manager {
+    force_conflicts = true
+  }
+
+  manifest = yamldecode(templatefile(
+    "${path.module}/resources/argocd-root-app.yaml", {}
+  ))
+
+  depends_on = [
+    helm_release.argocd,
+  ]
+}
